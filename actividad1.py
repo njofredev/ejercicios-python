7ff2c407d5884a84006b9e3c562804bf92842306
njofredev <nico.jofreandrade@gmail.com>
2024-02-06 21:18:01 -0300

    Actividad 1 resuelta

---
+++ actividad1.py
@@ -0,0 +1,11 @@
+# Datos
+a = [5, 1, 4, 9, 0]
+b = list(range(3, 10)) +list(range (20, 23))
+c = [[1,2], [3, 4, 5], [6,7]]
+d = ["perro", "gato", "jirafa", "elefante"]
+e = ["a", a, 2*a]
+
+# Resultados
+print(complex(b[0], b[1]))
+print(type(complex(b[0], b[1])))
+
