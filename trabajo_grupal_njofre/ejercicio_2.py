805183af604e4d2d8cab26e6c32eb9e51412532f
njofredev <nico.jofreandrade@gmail.com>
2024-03-09 10:28:20 -0300

    Add instructions

---
+++ trabajo_grupal_njofre/ejercicio_2.py
@@ -0,0 +1,6 @@
+"""
+Ejercicio grupal 2 - Trabajo grupal módulo 2
+njofre
+
+- Crear un programa que permita a una empresa de cine almacenar en su base de datos información sobre nuevas peliculas y estrenos. Cada pelicula se compone de: título, director, género y duración. Al final se debe mostrar la información detallada de cada película, lo que se debe crear la clase Pelicula y el Usuario encargado de agregar al menos 5 peliculas para después mostrarlas por pantalla.
+"""
